c7c3296971167c2446bf8b13e13bbc1605475c74
hendmissonen <hendmissonen@gmail.com>
2023-09-26 13:55:37 +0300

    some features were added

---
+++ main.js
@@ -0,0 +1,74 @@
+const startButton = document.querySelector("#startButton");
+const endButton = document.querySelector("#endButton");
+const circles = document.querySelectorAll(".circle");
+const scoreDisplay = document.querySelector(".score");
+
+let score = 0;
+let timer;
+let pace = 1000;
+let active = 0;
+let rounds = 0;
+
+const generateNumbers = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const clickCircle = (i) => {
+  if (i !== active) {
+    return endGame();
+  }
+
+  rounds--;
+  score += 10;
+  scoreDisplay.textContent = score;
+};
+
+circles.forEach((circle, i) => {
+  circle.addEventListener("click", () => clickCircle(i));
+});
+
+const enableEvents = () => {
+  circles.forEach((circle) => {
+    circle.style.pointerEvents = "auto";
+  });
+};
+
+const startGame = () => {
+  if (rounds >= 3) {
+    return endGame();
+  }
+
+  enableEvents();
+
+  const newActive = pickNew(active);
+
+  circles[newActive].classList.toggle("active");
+
+  circles[active].classList.remove("active");
+
+  active = newActive;
+
+  timer = setTimeout(startGame, pace);
+
+  pace -= 10;
+  rounds++;
+
+  function pickNew(active) {
+    const newActive = generateNumbers(0, 3);
+    if (newActive !== active) {
+      return newActive;
+    }
+  }
+  return pickNew(active);
+};
+
+const endGame = () => {
+  console.log("end game");
+  clearTimeout(timer);
+  resetGame();
+};
+const resetGame = () => {
+  window.location.reload();
+};
+startButton.addEventListener("click", startGame);
+endButton.addEventListener("click", endGame);
