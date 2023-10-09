e2a081a24bc90377c0145a5639d9f2a1edd6e055
hendmissonen <hendmissonen@gmail.com>
2023-10-09 18:21:10 +0300

    game update

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ const startButton = document.querySelector("#startButton");
 const endButton = document.querySelector("#endButton");
 const circles = document.querySelectorAll(".circle");
 const scoreDisplay = document.querySelector(".score");
+const muteButton = document.querySelector(".mute-button");
+
+const overlay = document.querySelector(".overlay");
+const paraz = document.querySelector(".one");
+const parazz = document.querySelector(".two");
+const parazzz = document.querySelector(".three");
+const closeButton = document.querySelector(".close");
+const modalScore = document.querySelector(".modal-score");
 
 let score = 0;
 let timer;
 let pace = 1000;
 let active = 0;
 let rounds = 0;
+const Music = new Audio("Game A.mp3");
 
 const generateNumbers = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -17,10 +26,27 @@ const clickCircle = (i) => {
   if (i !== active) {
     return endGame();
   }
+  let mySound = new Audio("Mario Jump - QuickSounds.com.mp3");
+  mySound.play();
+  mySound.volume = 0.1;
 
   rounds--;
+
   score += 10;
+
+  modalScore.textContent = score;
   scoreDisplay.textContent = score;
+
+  if (score <= 30) {
+    document.querySelector(".final-msg").textContent =
+      "I believe in you 🤥.Try harder 😬 ";
+  } else if (score > 30 && score < 50) {
+    document.querySelector(".final-msg").textContent =
+      "Average 🫢.Keep going 🤛";
+  } else {
+    document.querySelector(".final-msg").textContent =
+      "Awesome 🥳, you did it 🤪";
+  }
 };
 
 circles.forEach((circle, i) => {
@@ -38,6 +64,9 @@ const startGame = () => {
     return endGame();
   }
 
+  endButton.style.display = "block";
+  startButton.style.display = "none";
+
   enableEvents();
 
   const newActive = pickNew(active);
@@ -47,28 +76,57 @@ const startGame = () => {
   circles[active].classList.remove("active");
 
   active = newActive;
+  console.log(active);
+  console.log(newActive);
 
   timer = setTimeout(startGame, pace);
 
   pace -= 10;
   rounds++;
+  if (rounds > 3) {
+    document.querySelector(".final-msg").textContent = "try u";
+  }
 
   function pickNew(active) {
     const newActive = generateNumbers(0, 3);
     if (newActive !== active) {
       return newActive;
     }
+    return pickNew(active);
   }
-  return pickNew(active);
 };
 
 const endGame = () => {
-  console.log("end game");
+  let mySound = new Audio("Mario Death - QuickSounds.com.mp3");
+  mySound.play();
   clearTimeout(timer);
-  resetGame();
+  modalShow();
+  stopMusic();
 };
+
 const resetGame = () => {
   window.location.reload();
 };
-startButton.addEventListener("click", startGame);
-endButton.addEventListener("click", endGame);
+
+const modalShow = () => {
+  overlay.classList.toggle("visible");
+};
+
+const playMusic = () => {
+  Music.play();
+};
+
+const playMusicWithGame = () => {
+  playMusic();
+  startGame();
+};
+
+const stopMusic = () => {
+  Music.pause();
+};
+
+startButton.addEventListener("click", playMusicWithGame);
+endButton.addEventListener("click", resetGame);
+closeButton.addEventListener("click", resetGame);
+overlay.addEventListener("click", resetGame);
+muteButton.addEventListener("click", stopMusic);
